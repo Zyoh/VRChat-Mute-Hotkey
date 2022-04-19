6bb35d103f5815aa699e6d38b5ca9a5e81e2e766
Zoe <zoe@zyoh.ca>
2022-04-18 23:44:51 -0400

    CLI args

--- src/vrchat_mute_hotkey.py
+++ src/vrchat_mute_hotkey.py
@@ -1,19 +1,20 @@
 import keyboard
 from pythonosc import udp_client
 import time
+import argparse
 
 
 class VRChatMuteHotkey:
-	def __init__(self, sender_ip: str = None, sender_port: int = None, hotkey: str = None) -> None:
-		if sender_ip is None:
-			sender_ip = "127.0.0.1"
+	def __init__(self, ip: str = None, sender_port: int = None, hotkey: str = None) -> None:
+		if ip is None:
+			ip = "127.0.0.1"
 		if sender_port is None:
 			sender_port = 9000
 		if hotkey is None:
 			hotkey = "F8"
 	
 		self.hotkey = hotkey
-		self.client = udp_client.SimpleUDPClient(sender_ip, sender_port)
+		self.client = udp_client.SimpleUDPClient(ip, sender_port)
 
 	def toggle_mute(self):
 		self.client.send_message("/input/Voice", 0)
@@ -29,7 +30,13 @@ class VRChatMuteHotkey:
 
 
 def main():
-	app = VRChatMuteHotkey()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--ip", type=str, default="127.0.0.1", help="IP address of the device running VRChat. Defaults to this device.")
+	parser.add_argument("--sender-port", type=int, default=9000, help="Port to send to.")
+	parser.add_argument("--hotkey", type=str, default="F8", help="Hotkey to toggle voice with.")
+	args = parser.parse_args()
+
+	app = VRChatMuteHotkey(ip=args.ip, sender_port=args.sender_port, hotkey=args.hotkey)
 	app.run()
 
 if __name__ == "__main__":
